cd1abdd8003daca89718696d57c6e99c83e80331
Jimikumar Chhatrala <jimichhatrala8488@gmail.com>
2022-07-30 03:20:52 +0530

    Router - dish,leader,promotions

--- node-express/index.js
+++ node-express/index.js
@@ -8,6 +8,10 @@ const bodyParser = require("body-parser");
 
 const dishRouter = require("./routes/dishRouter");
 
+const promoRouter = require("./routes/promoRouter");
+
+const leaderRouter = require("./routes/leaderRouter");
+
 const hostname = "localhost";
 
 const port = 3000;
@@ -21,29 +25,8 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 
 app.use("/dishes", dishRouter);
-
-app.get("/dishes/:dishId", (req, res, next) => {
-  res.end("Will send details of the dish: " + req.params.dishId + " to you!");
-});
-
-app.post("/dishes/:dishId", (req, res, next) => {
-  res.statusCode = 403;
-  res.end("POST operation not suppored on /dishes/" + req.params.dishId);
-});
-
-app.put("/dishes/:dishId", (req, res, next) => {
-  res.write("Updating the dish: " + req.params.dishId + "\n");
-  res.end(
-    "Will update the dish: " +
-      req.body.name +
-      " with details: " +
-      req.body.description
-  );
-});
-
-app.delete("/dishes/:dishId", (req, res, next) => {
-  res.end("Deleting dish: " + req.params.dishId);
-});
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
 
 app.use((req, res, next) => {
   res.statusCode = 200;
